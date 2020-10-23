a84f4384d4f0b058704dec230534ec8074a9b1d6
Marta Tomczak <49766582+mtomchuck@users.noreply.github.com>
2020-10-23 13:41:46 +0200

    adding labels

--- src/components/MessageForm/index.js
+++ src/components/MessageForm/index.js
@@ -4,10 +4,14 @@ function MessageForm({ message, handleSubmit, handleContentChange }) {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="user" />
+        <input
+          type="text"
+          name="user"
+          placeholder="username" />
         <input
           type="text"
           name="content"
+          placeholder="message"
           value={message}
           onChange={(event) => handleContentChange(event.target.value)}
         />
